f01eff92e2a79e26791f3a1b2ed459a01c6fab3f
brandon <brandonmarion@billericak12.com>
2019-09-23 12:30:03 -0400

    johnny rockets

---
+++ src/com/company/MathEx.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import static java.util.Collections.swap;
+
+public class MathEx {
+    public static void main(String [] args) {
+        int a;
+        int a = 234;
+        return swap(a);
+
+    }
+}

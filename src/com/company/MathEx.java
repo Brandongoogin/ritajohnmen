4274bdcb8381fc8289d0ecb998f70c2a30f9751f
brandon <brandonmarion@billericak12.com>
2019-09-30 12:28:57 -0400

    googclass

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -6,7 +6,6 @@ public class MathEx {
         int a = x % 100 / 10;
         int b = x / 100;
         int c = b * 100 + y * 10 + a;
-
         return c;
     }
 
@@ -15,18 +14,35 @@ public class MathEx {
         System.out.print(z);
         timeLeft(1, 15, 4, 36);
        timeLeft(1, 34, 8, 20);
-        }
-     private static int timeLeft(int curHour, int curMin, int depHour, int depMin){
-        int d = depHour * 60;
-        d = d + depMin;
-        int e = curHour * 60;
-        e = e + curMin;
-        int f = d - e;
-        int g = f/60;
-        int h = f%60;                                                                                        
-        System.out.print(d + " hours ");
-        System.out.println(e + " minutes ");
+       int j = dayOfWeek(0,1);
+       System.out.println(j);
+       j = dayOfWeek(0,14);
+       System.out.println(j);
+        j = dayOfWeek(6,22);
+        System.out.println(j);
+        j = dayOfWeek(5,4);
+        System.out.println(j);
+        j = dayOfWeek(1,24);
+        System.out.println(j);
+        j = dayOfWeek(2,1);
+        System.out.println(j); 
 
+        }
+     private static int timeLeft(int curHour, int curMin, int depHour, int depMin) {
+         int d = depHour * 60;
+         d = d + depMin;
+         int e = curHour * 60;
+         e = e + curMin;
+         int f = d - e;
+         int g = f / 60;
+         int h = f % 60;
+         System.out.println(g + " hours " + h + " minutes ");
+         return d;
+     }
+     private static int dayOfWeek(int jan1, int date){
+        int i = (date + jan1) - 1;
+        i = i%7;
+        return i;
     }
 
 }
